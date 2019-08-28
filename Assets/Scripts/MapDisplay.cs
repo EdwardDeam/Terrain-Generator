f74cbedd83660e1db2f5d5c32942e48b90c8d101
Edward Deam <edwardcrdeam@gmail.com>
2019-08-28 22:43:35 +1000

    Refactore texture generation to add color to map

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -6,29 +6,11 @@ public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRenderer;
 
-    public void DrawNosieMap(float[,] noiseMap)
+    public void DrawTexture(Texture2D texture)
     {
-        int width = noiseMap.GetLength(0);
-        int height = noiseMap.GetLength(1);
-
-        Texture2D texture = new Texture2D(width, height);
-
-        // It is faster to create a color array and assign all the pixels at once rather than assigning then individually.
-        Color[] colorMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                // Convert 2D array index to 1D array index
-                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-            }
-        }
-        texture.SetPixels(colorMap);
-        texture.Apply();
-
         // Material is only generated at runtime, need to use shared material to view changes in the editor.
         textureRenderer.sharedMaterial.mainTexture = texture;
         // Set the size of the plane to the same as the map.
-        textureRenderer.transform.localScale = new Vector3(width, 1, height);
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
     }
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color color;
+}
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode { NoiseMap, ColorMap };
+    public DrawMode drawMode;
+
     [SerializeField]
     private int mapHeight;
     [SerializeField]
@@ -26,13 +35,36 @@ public class MapGenerator : MonoBehaviour
     [SerializeField]
     private Vector2 offset;
 
+    [SerializeField]
+    private TerrainType[] terrains;
 
     public void GenerateMap ()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, scale, octaves, persistance, lacunarity, offset);
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float locationHeight = noiseMap[x, y];
+                for (int i = 0; i < terrains.Length; i++) {
+                    // Found the terrain that this locations height matches.
+                    if(locationHeight <= terrains[i].height) {
+                        // Convert 2D array index to 1D array index
+                        colorMap[y * mapWidth + x] = terrains[i].color;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNosieMap(noiseMap);
+
+        if(drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColorMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
     }
 
     // Called when one of the variables is changed in the inspector.
